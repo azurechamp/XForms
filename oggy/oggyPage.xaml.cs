c34fadabf355f2e8c83cd986df5f80d0cfc9d2a7
Saad Mahmood <saad-mehmood@outlook.com>
2016-11-28 18:27:55 +0500

    Initial check-in of module oggy

---
+++ oggy/oggyPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Xamarin.Forms;
+
+namespace oggy
+{
+	public partial class oggyPage : ContentPage
+	{
+		public oggyPage()
+		{
+			InitializeComponent();
+		}
+
+		void Handle_Clicked(object sender, System.EventArgs e)
+		{
+		
+		}
+	}
+}
